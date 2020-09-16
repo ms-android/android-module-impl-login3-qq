7f195e81af4a0afda6724cc918ec12a4326aeb16
m_maohuawei <maohuawei@bdlbsc.com>
2020-09-16 17:13:46 +0800

    init

---
+++ sdk/src/main/java/com/ms/module/impl/login3/qq/QQImpl.java
@@ -0,0 +1,15 @@
+package com.ms.module.impl.login3.qq;
+
+import android.app.Activity;
+
+import com.ms.module.supers.inter.login3.IQQLoginAdapter;
+import com.ms.module.supers.inter.login3.LoginCallBack;
+
+public class QQImpl extends IQQLoginAdapter {
+
+
+    @Override
+    public void login(Activity activity, LoginCallBack callBack) {
+        super.login(activity, callBack);
+    }
+}
